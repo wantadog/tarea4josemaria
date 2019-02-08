ff06e01c78dc7a0d802a3f376e6b7cd93f2981cd
elkillooo <diegosanchezleon@gmail.com>
2019-02-08 13:19:24 +0100

    Añade clase main

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author diego
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Absenta abs1 = new Absenta(13, 70);
+        Absenta abs2 = new Absenta(23, 85);
+
+        System.out.println(abs1.toString());
+        System.out.println(abs1.toString());
+    }
+}
